29fea711f3bb8e9236d7bc4c35a86384f7287f29
ladew222 <egweinbe@gmail.com>
2023-09-01 14:18:06 +0000

    first

---
+++ script.js
@@ -0,0 +1,35 @@
+// Function to simulate user input
+function getUserInput() {
+    return prompt('Enter a binary number: ');
+  }
+  
+  // Function to display output
+  function displayOutput(result) {
+    console.log('Result:', result);
+  }
+  
+  // Task 1: Binary to Decimal Conversion
+  function binaryToDecimal(binary) {
+    // Write your code here to convert binary to decimal
+    // Return the decimal value
+  }
+  
+  // Task 2: Decimal to Binary Conversion
+  function decimalToBinary(decimal) {
+    // Write your code here to convert decimal to binary
+    // Return the binary number as a string
+  }
+  
+  // Task 3: Binary Addition
+  function binaryAddition(binary1, binary2) {
+    // Write your code here to perform binary addition
+    // Return the result of binary addition
+  }
+  
+  // Export functions for testing
+  module.exports = {
+    binaryToDecimal,
+    decimalToBinary,
+    binaryAddition,
+  };
+  
